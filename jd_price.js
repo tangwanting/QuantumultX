339c9070c4d4172d0f9a63e8fc95d9bc0e3ab34a
tangwanting <tangwanting@gmail.com>
2021-05-22 07:09:55 +0800

    Update jd_price.js

--- jd_price.js
+++ jd_price.js
@@ -63,7 +63,7 @@ if (url.indexOf(path2) != -1) {
 
 function priceSummary(data) {
 	data = data.series[0]
-	let summary = `🍵 当前: ${parseFloat(data.current / 100.0)}${getSpace(8)}最低: ${parseFloat(data.min / 100.0)}${getSpace(8)}最高: ${parseFloat(data.max / 100.0)}`;
+	let summary = `当前到手价: ${parseFloat(data.current / 100.0)}${getSpace(8)}最低: ${parseFloat(data.min / 100.0)}${getSpace(8)}最高: ${parseFloat(data.max / 100.0)}`;
 	const list = historySummary(data.data);
 	list.forEach((item, index) => {
 		summary += `\n${item.Name}${getSpace(8)}${item.Price}${getSpace(8)}${item.Date
